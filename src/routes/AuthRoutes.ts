9fb90b9f42048bc4bc5452890ad7fa8f27e61940
Tbrainyz <gbadebo.wale004@gmail.com>
2026-06-17 16:34:56 +0100

    test

--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -12,5 +12,11 @@ router.post("/register",              registerUser);
 router.post("/login",                 loginUser);
 router.post("/forgot-password",       forgotPassword);
 router.put("/reset-password/:token",  resetPassword);
+router.get("/test", (req, res) => {
+  res.json({
+    success: true,
+    message: "Auth routes are working",
+  });
+});
 
 export default router;
